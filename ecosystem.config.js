0ddb4b1d5d8c4199ea8b6778f2cce5046c7ef025
Alberto <apenam@unamad.edu.pe>
2026-02-02 10:13:11 -0500

    segundo plano ejecutandose

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "herbario",
+      script: "waitress-serve",
+      args: "--port=5000 app:app",
+      cwd: "C:/Apache24/htdocs/Herbario",
+      interpreter: "none",
+      watch: false,
+      autorestart: true,
+      max_restarts: 10,
+      env: {
+        PYTHONUNBUFFERED: "1"
+      }
+    }
+  ]
+};
